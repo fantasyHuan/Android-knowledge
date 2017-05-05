dba43de8150f88147f286ba51da1a6ea803d79b4
fantasyHuan <fantasy_Huan@qq.com>
2017-05-06 00:40:15 +0800

    添加知识：非生命周期的方法-->onSaveInstanceSate()和onRestoreInstanceState() 扩展知识：旋转屏幕所引发的情况

--- app/src/main/java/com/huan/android_xuexi/_Activity.java
+++ app/src/main/java/com/huan/android_xuexi/_Activity.java
@@ -1,11 +1,8 @@
 package com.huan.android_xuexi;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 
 /**
  * Created by huan on 2017/4/28.
@@ -40,12 +37,20 @@ public class _Activity  extends Activity{
  * --结束状态（Killed）Activity被移除
  *
  *
+ *当旋转屏幕方向时，将会调用：
+ *onPause()-->onSaveInstanceState()-->onStop()-->onDestroy()-->销毁Activity
+ * 销毁后生成新Activity-->onCreate()-->onStart()-->onRestoreInstanceState()-->onResume()
+ *
+ * 旋转屏幕方向系统销毁Activity，并调用onSaveInstanceState()保存状态数据，
+ * 在屏幕切换之后重新创建Activity，并调用onRestoreInstanceState()恢复状态数据。
+ * 注：onSaveInstanceState()和onRestoreInstanceState()并不属于Activity生命周期的方法
  */
 
     /**
      *创建：开始
      * 每次启动一个新的Activity，都将会先调用此方法
-     * @param savedInstanceState
+     * @param savedInstanceState-->指Activity当前状态信息
+     * 状态数据以key-value的形式放入到savedInstanceState·中
      */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -106,12 +111,27 @@ public class _Activity  extends Activity{
         super.onRestart();
         Log.i("tag","调用了-->onRestart");
     }
-    protected void dak(View view){
-        Dialog dialog=new AlertDialog.Builder(_Activity.this)
-                .setTitle("一个对话框让人失去焦点的←_←")
-                .setPositiveButton("哦",null)
-                .create();
-        dialog.show();
 
+    /**
+     *当发生意外，由系统"未经许可"销毁Activity时，此方法会被调用（保存状态数据）
+     *方法主要用于保存状态数据（各种UI控件的状态并且前提下控件拥有唯一指定的ID属性）
+     * 当屏幕方向切换时，系统会销毁Activity，并调用此方法保存状态数据，在屏幕切换之后重新创建Activity，
+     * 并调用onRestoreInstanceState()恢复状态数据。
+     * @param outState
+     */
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i("tag","调用了-->onSaveInstanceState");
     }
+
+    /**
+     *恢复状态数据
+     * @param savedInstanceState
+     */
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i("tag","调用了-->onRestoreInstanceState");
+    }
+
+
 }
